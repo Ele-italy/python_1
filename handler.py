7e5c05daf6b87efb2ff6d71fbe6dbe84181c931d
Ele-italy <helena.kozlova@tut.by>
2021-08-13 19:09:34 +0300

    change file

---
+++ handler.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from task_3_6 import drink
+
+
+app = FastAPI()
+
+
+@app.put("/task/{a}")
+async def handler(a:int):
+    r = drink(a)
+    return {"result":str(r)}
+   
+
+
+
+
+
+
+
+
+
+
