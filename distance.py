1b65e75572cc358b3ac4d5b0f2a2c2d74bb13253
Ele-italy <helena.kozlova@tut.by>
2021-08-17 20:42:03 +0300

    'add get hello world'

--- distance.py
+++ distance.py
@@ -4,10 +4,13 @@ from my_function import main
 
 app = FastAPI()
 
+@app.get("/")
+async def handler ():
+    return "hello world"
 
 @app.get("/distance/{a}/{b}")
-async def handler(a:str,b:str ):
-    r = main(a,b)
+async def handler (a:str, b:str ):
+    r = main(a, b)
     return {"result":str(r)}
    
 
